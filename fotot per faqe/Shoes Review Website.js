055a02c401fdabe15511bde270974980d6aa24c7
Blend06 <bk67418@ubt-uni.net>
2023-11-29 15:37:30 +0100

    fdfd

---
+++ fotot per faqe/Shoes Review Website.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Email and password are required");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password should be at least 6 characters");
+        return false;
+    }
+
+    return true;
+}
+
