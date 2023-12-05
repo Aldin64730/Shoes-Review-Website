4ec00ca5706d8b9be6980aa92f38ee172c1169ab
Blend06 <bk67418@ubt-uni.net>
2023-12-05 18:20:26 +0100

    sdsd

---
+++ Materiali/Shoes Review Website.js
@@ -0,0 +1,28 @@
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Email and password are required");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password should be at least 6 characters");
+        return false;
+    }
+
+    return true;
+}
+
+document.getElementById('RegisterButton').addEventListener('click', function() {
+    var registrationForm = document.getElementById('registrationForm');
+    registrationForm.style.display = (registrationForm.style.display === 'none') ? 'block' : 'none';
+});
+
+document.getElementById('RegisterForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+
+
