ba63b5232b49e2310b6958f35052d8a348ed3a44
Blend06 <bk67418@ubt-uni.net>
2023-12-07 09:38:11 +0100

    fggf

--- Materiali/Shoes Review Website.js
+++ Materiali/Shoes Review Website.js
@@ -24,5 +24,21 @@ document.getElementById('RegisterForm').addEventListener('submit', function(even
     event.preventDefault();
 });
 
+function alidavteRegisterForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Email and password are required");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password should be at least 6 characters");
+        return false;
+    }
+
+    return true;
+}
 
 
