b65c9bb541a65b0f68706ee7e010bfd5eab68735
Aldin64730 <as64730@ubt-uni.net>
2023-12-25 17:11:38 +0100

    validimi regjister form

--- Materiali/registerformjs.js
+++ Materiali/registerformjs.js
@@ -1,6 +1,7 @@
 function validateForm() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    var user = document.getElementById('user').value;
 
     if (email.trim() === "" || password.trim() === "") {
         alert("Email and password are required");
@@ -11,6 +12,11 @@ function validateForm() {
         alert("Password should be at least 6 characters");
         return false;
     }
-    alert("You have logged in successfully!");
+
+    if (user.length < 4) {
+        alert("Username should be at least 4 characters");
+        return false;
+    }
+    alert("You have registered successfully! Please return to the home page!");
     return true;
 }
